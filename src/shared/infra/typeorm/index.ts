55452dcd98156d92237e21fc271eb267a6b13774
Daniel Sobrinho <danielsobrinho.dev@gmail.com>
2022-04-08 22:13:30 -0300

    create admin seed

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,3 +1,14 @@
-import { createConnection } from "typeorm";
+// import { createConnection } from "typeorm";
+// createConnection();
 
-createConnection();
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
+
+export default async (host = "database"): Promise<Connection> => {
+    const defaultOption = await getConnectionOptions();
+
+    return createConnection(
+        Object.assign(defaultOption, {
+            host,
+        })
+    );
+};
